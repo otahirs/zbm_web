b5ae10889be0e76ca5928e10b9941b355b002c8f
otahirs <otakar.hirs@gmail.com>
2019-09-26 19:57:52 +0200

    logs v0.1

--- plugins/PHP/PHP.php
+++ plugins/PHP/PHP.php
@@ -1,12 +1,16 @@
 <?php
 namespace Grav\Plugin;
+use Grav\Common\Grav;
+use Grav\Common\Helpers\LogViewer;
 use \Grav\Common\Plugin;
 class PHPPlugin extends Plugin
 {
     public static function getSubscribedEvents()
     {
         return [
-            'onTwigExtensions' => ['onTwigExtensions', 0]
+            'onTwigExtensions' => ['onTwigExtensions', 0],
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
+			'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
         ];
     }
     public function onTwigExtensions()
@@ -14,5 +18,31 @@ class PHPPlugin extends Plugin
         require_once(__DIR__ . '/twig/PhpTwigExtension.php');
         $this->grav['twig']->twig->addExtension(new PhpTwigExtension());
     }
+
+    public function onTwigTemplatePaths()
+	{
+		// add templates to twig path
+		$this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+	}
+
+
+
+
+    public function onTwigSiteVariables()
+	{
+        // setup
+        $twig = $this->grav['twig'];
+		$page = $this->grav['page'];
+
+		// only load the vars if calendar page
+		if ($page->template() == 'logs')
+        {			// add calendar to twig as calendar
+            
+            $twig->twig_vars['logviewer'] = new LogViewer();
+		}
+
+
+	}
+
 }
 ?>
